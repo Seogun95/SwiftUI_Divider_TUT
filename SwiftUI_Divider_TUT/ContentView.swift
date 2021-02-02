cd2c9eb09d32f223ed93ce6c23aa50be7be44b76
SeonJungKim <ksj083895@gmail.com>
2021-02-02 12:09:39 +0500

    Divider

--- SwiftUI_Divider_TUT/ContentView.swift
+++ SwiftUI_Divider_TUT/ContentView.swift
@@ -9,10 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //VStack의 간격을 40으로 줌
+        VStack(spacing: 40){
+            Group {
+                //1. H_Divider
+                Text("Horizontal Divider")
+                Divider()
+                Image(systemName: "a.circle.fill")
+                Divider()
+            }
+            
+            Group {
+                //2. V_Divider
+                Text("Vertical Divider")
+                HStack {
+                    Divider()
+                    Image(systemName: "b.circle.fill")
+                    Divider()
+                }.frame(height: 100)
+            }
+            
+            Group {
+                //3. 색상
+                Text("Divider 색상")
+                Divider()
+                    .background(Color.red)
+                Image(systemName: "c.circle.fill")
+                Divider()
+                    .background(Color.red)
+            }
+            
+            Group {
+                //4. 길이
+                Text("Driver 길이 조절")
+                Divider()
+                    .frame(width: 200)
+                Image(systemName: "d.circle.fill")
+                Divider()
+                    .frame(width: 300)
+            }
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
